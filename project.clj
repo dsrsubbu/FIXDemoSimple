82227c8dbdbcfa8a8b68fb7dc4ac4922d0cfe2ff
Stefan Wendt <S@wf0346.dip.tu-dresden.de>
2015-01-26 17:39:25 +0100

    Adding description and URL to project.clj

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject fix-demo-simple "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "FIXDemoSimple demonstrates how to build messages and applications with QuickFIX/J. A sample requester and a provider can be started as Clojure applications with Leiningen."
+  :url "https://bitbucket.org/stefan_we/fixdemosimple/"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
